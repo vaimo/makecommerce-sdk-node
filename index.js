685642e28eb35e2538ca92ce3b5cac5aced88e8c
Oliver Soop <oliversoop@gmail.com>
2019-04-13 12:35:28 +0300

    Add payment gateway link creation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Makecommerce = require(__dirname+'/lib/makecommerce.js');
+const GatewayPayment = require(__dirname+'/lib/payment.js');
 
-module.exports = Makecommerce;
+module.exports = {
+  Makecommerce: Makecommerce,
+  GatewayPayment: GatewayPayment
+};
 
